f81898faa5ad896d6fff31d17fe0c0004d3dffbe
zigo <zigotero@gmail.com>
2021-12-06 23:13:25 -0500

    enrollement process inscrito en eureka

--- enrollment-process/src/main/java/com/example/enrollmentprocess/EnrollmentProcessApplication.java
+++ enrollment-process/src/main/java/com/example/enrollmentprocess/EnrollmentProcessApplication.java
@@ -2,8 +2,10 @@ package com.example.enrollmentprocess;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EnrollmentProcessApplication {
 
 	public static void main(String[] args) {
