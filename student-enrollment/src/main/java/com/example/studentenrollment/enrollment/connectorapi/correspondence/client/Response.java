360c0d1fee79b31aefcfb1e97fb1b2813d50dde4
zigo1 <jashirchirre@gmail.com>
2021-12-21 18:43:57 -0500

    fix de correspondece

--- student-enrollment/src/main/java/com/example/studentenrollment/enrollment/connectorapi/correspondence/client/Response.java
+++ student-enrollment/src/main/java/com/example/studentenrollment/enrollment/connectorapi/correspondence/client/Response.java
@@ -1,12 +1,12 @@
 package com.example.studentenrollment.enrollment.connectorapi.correspondence.client;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Response {
 
     private String message;
